6687fa738d333e9b18e801161a84a8e214e2dda2
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2019-05-31 14:54:30 +0100

    Add files via upload

---
+++ Source Code/GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Database Query Scheduler")]
+[assembly: AssemblyDescription("Allow you to create scheduled tasks to query databases")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mutu Adi-Marian")]
+[assembly: AssemblyProduct("Database Query Scheduler")]
+[assembly: AssemblyCopyright("Copyright ©2018 - Edward Bennett")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("431d702c-f791-44fa-8783-f9e18bf31946")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
